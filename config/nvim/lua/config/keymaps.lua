e12504d977438093c5d72efef47f50c913d9ae68
Eleven <eteplus@outlook.com>
2025-09-19 10:01:51 +0800

    chore: update nvim plugin versions and vscode integration config
    - updated plugin commit hashes in lazy-lock.json
    - added lazyvim.plugins.extras.vscode to lazyvim.json
    - updated news version from 10960 to 11866
    - commented out redundant space keymap for vscode integration

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -6,7 +6,7 @@ local keymap = vim.keymap
 
 if vim.g.vscode then
   keymap.set({ "n", "v" }, "gcc", [[<cmd>call VSCodeNotify('editor.action.commentLine')<cr>]], { silent = true })
-  keymap.set({ "n", "v", "x" }, "<space>", [[<cmd>call VSCodeNotify('whichkey.show')<cr>]], { silent = true })
+  -- keymap.set({ "n", "v", "x" }, "<space>", [[<cmd>call VSCodeNotify('whichkey.show')<cr>]], { silent = true })
   keymap.set("n", "u", [[<cmd>call VSCodeNotify('undo')<cr>]], { silent = true })
   keymap.set("n", "<Esc>", "<Esc>:noh<CR>", { noremap = true, silent = true })
 end
